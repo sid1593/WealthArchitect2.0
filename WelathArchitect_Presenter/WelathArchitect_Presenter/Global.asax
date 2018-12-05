fa8f0b57a3ff4038a3f394b868addffec04d75e0
Siddharth Somvanshi <sid1593@gmail.com>
2018-12-05 09:57:23 +0530

    Initial

---
+++ WelathArchitect_Presenter/WelathArchitect_Presenter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WelathArchitect_Presenter.MvcApplication" Language="C#" %>
---
+++ WelathArchitect_Presenter/WelathArchitect_Presenter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WelathArchitect_Presenter.Startup))]
+namespace WelathArchitect_Presenter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
